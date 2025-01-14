06e2f13c3a4ca27381512bb0231ddea63ccf481f
Leonardo D. Cobaleda <116767849+leonardocoba@users.noreply.github.com>
2025-01-14 11:51:10 -0500

    Initial Commit

---
+++ Screen Therapy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Screen Therapy
+//
+//  Created by Leonardo Cobaleda on 1/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Screen Therapy/Screen_TherapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_TherapyApp.swift
+//  Screen Therapy
+//
+//  Created by Leonardo Cobaleda on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Screen_TherapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
